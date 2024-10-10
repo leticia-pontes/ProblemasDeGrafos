c794a917f24a7ded0b08d1d91c19d1184279a8e7
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2024-10-09 21:41:58 -0300

    Cria exercicio - Problema do Troco

---
+++ exercicios/problema_do_troco.py
@@ -0,0 +1,25 @@
+MOEDAS = [100, 25, 10, 5, 1]
+
+def ordena_moedas(moedas):
+    return sorted(moedas, reverse=True)
+
+def problema_do_troco(valor):
+    valor = int(valor)
+
+    moedas_ordenadas = ordena_moedas(MOEDAS)
+    moedas_de_troco = []
+
+    for moeda in moedas_ordenadas:
+        quantidade_moedas = 0
+        if valor >= moeda:
+            quantidade_moedas = valor // moeda
+            valor -= quantidade_moedas * moeda
+        moedas_de_troco.append(f'{quantidade_moedas} moeda(s) de {moeda}')
+    
+    print('\n'.join(moedas_de_troco))   
+
+try:
+    valor = int(input('Insira o valor para troco: '))
+    problema_do_troco(valor)
+except:
+    print('Tipo do valor inválido')
